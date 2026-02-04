cd36d3e7fa130101c5a8a63286acdb6d0b4eb64b
Leah Attebury <leahnyx@proton.me>
2026-02-04 10:46:43 -0700

    I can make midi files beotch

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
-#include <cmath>
-#include <iostream>
-#include <random>
+#include <libremidi/libremidi.hpp>
+#include <libremidi/writer.hpp>
 
+#include <fstream>
+#include <random>
 
 std::random_device dev;
 std::mt19937 rng(dev());
-std::uniform_real_distribution<std::double_t> generator{0,1};
+std::uniform_int_distribution<std::int8_t> generator{0,72};
 
-std::double_t getRandom()
+std::int8_t getRandomNotes()
 {
     return generator(rng);
 }
 
 int main(int argc, char** argv)
 {
-    getRandom();
+    libremidi::writer writer;
+
+    writer.tracks.push_back(
+    libremidi::midi_track{
+        libremidi::track_event{0, 0, libremidi::channel_events::note_on(1, 45, 35)},
+        libremidi::track_event{140, 0, libremidi::channel_events::note_off(1, 45, 0)},
+    }
+    );
+
+    {
+        int tick = 500;
+        int track = 3;
+        libremidi::message msg = libremidi::channel_events::note_on(1, 45, 35);
+
+        writer.add_event(tick, track, msg);
+    }
+
+    std::ofstream output{"midi/output.mid", std::ios::binary};
+    writer.write(output);
 }
