ed291cf79730acec06ac2070c5dd4f5e0f4300eb
Leah Attebury <leahnyx@proton.me>
2026-02-04 11:59:15 -0700

    Randomly generated, short, midi files now

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include <fstream>
 #include <random>
 
-std::random_device                         dev;
-std::mt19937                               rng(dev());
-std::uniform_int_distribution<std::int8_t> generator {0, 72};
+std::random_device                           dev;
+std::mt19937                                 rng(dev());
+std::uniform_int_distribution<std::uint8_t>  noteGenerator {0, 72};
+std::uniform_int_distribution<std::uint8_t>  velocityGenerator {0, 255};
+std::uniform_int_distribution<std::uint16_t> durationGenerator {1, 4};
 
-std::int8_t getRandomNotes() { return generator(rng); }
+std::uint8_t getRandomNote() { return noteGenerator(rng); }
+
+std::uint8_t getRandomVelocity() { return velocityGenerator(rng); }
+
+std::uint16_t getRandomDuration() { return durationGenerator(rng); }
 
 int main(int argc, char** argv)
 {
@@ -22,9 +28,10 @@ int main(int argc, char** argv)
     });
 
     {
-        int                tick  = 500;
-        int                track = 3;
-        libremidi::message msg = libremidi::channel_events::note_on(1, 45, 35);
+        std::uint16_t      tick  = getRandomDuration();
+        std::uint8_t       track = 3; // magic number for now
+        libremidi::message msg   = libremidi::channel_events::note_on(
+            1, getRandomNote(), getRandomVelocity());
 
         writer.add_event(tick, track, msg);
     }
